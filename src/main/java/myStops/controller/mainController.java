ff890ab171013216fd6f2ba8be367486bc5cdbcc
Oodi <simo.auvinen@helsinki.fi>
2016-09-24 20:43:45 +0300

    Project base

---
+++ src/main/java/myStops/controller/mainController.java
@@ -0,0 +1,19 @@
+package myStops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Simo on 24.9.2016.
+ */
+@Controller
+@ResponseBody
+public class mainController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hei ";
+    }
+}
